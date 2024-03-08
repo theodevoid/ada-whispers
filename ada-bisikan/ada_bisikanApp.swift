c14c26ddece1e6dbe0240e2917473f9a212f80d9
theodevoid <voidfnc9@gmail.com>
2024-03-08 22:16:46 +0700

    Initial Commit

---
+++ ada-bisikan/ada_bisikanApp.swift
@@ -0,0 +1,17 @@
+//
+//  ada_bisikanApp.swift
+//  ada-bisikan
+//
+//  Created by Theodore Mangowal on 08/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ada_bisikanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
